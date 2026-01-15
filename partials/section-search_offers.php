87d9242baa41a1a857f6e8ece6ef27e08cef54da
Julián Escobar <julianandres.escobar03@gmail.com>
2026-01-15 16:35:16 -0500

    feat(search-offers): add ACF visibility control for desktop and mobile

--- partials/section-search_offers.php
+++ partials/section-search_offers.php
@@ -1,17 +1,33 @@
 <?php 
-// Obtener los campos ACF
-$title = get_field('search_offers_title');
-$subtitle = get_field('search_offers_subtitle');
-$button_text = get_field('search_offers_button_text');
-$button_url = get_field('search_offers_button_url');
-$background = get_field('search_offers_background');
-?>
 
+$display     = get_field('search_offers_display'); 
+$title       = get_field('search_offers_title');
+$subtitle    = get_field('search_offers_subtitle');
+$button_text = get_field('search_offers_button_text');
+$button_url  = get_field('search_offers_button_url');
+$background  = get_field('search_offers_background');
+
+
+$show_section = false;
+
+if ($display === 'both') {
+    $show_section = true;
+} elseif ($display === 'mobile' && wp_is_mobile()) {
+    $show_section = true;
+} elseif ($display === 'desktop' && !wp_is_mobile()) {
+    $show_section = true;
+}
+
+if ($show_section) :
+?>
 <section 
-    class="search_offers" 
-    <?php if ($background): ?>style="background-image: url('<?php echo esc_url($background['url']); ?>');"<?php endif; ?>
+    class="search_offers"
+    <?php if ($background): ?>
+        style="background-image: url('<?php echo esc_url($background['url']); ?>');"
+    <?php endif; ?>
 >
     <div class="search_offers__container">
+
         <?php if ($title): ?>
             <h2 class="search_offers__title"><?php echo esc_html($title); ?></h2>
         <?php endif; ?>
@@ -21,10 +37,14 @@ $background = get_field('search_offers_background');
         <?php endif; ?>
 
         <?php if ($button_text): ?>
-            <a href="<?php echo $button_url ? esc_url($button_url) : '#'; ?>" class="search_offers__button">
+            <a 
+                href="<?php echo $button_url ? esc_url($button_url) : '#'; ?>" 
+                class="search_offers__button"
+            >
                 <?php echo esc_html($button_text); ?>
             </a>
         <?php endif; ?>
 
     </div>
 </section>
+<?php endif; ?>
