0883fa669357acc4071ea0a16f3eb280c1114524
Julián Escobar <julianandres.escobar03@gmail.com>
2026-01-16 16:59:51 -0500

    refactor: update front-page to use template-parts for sections

--- front-page.php
+++ front-page.php
@@ -9,13 +9,13 @@ get_header();
 ?>
 
 <main class="home">
-    <?php get_template_part('partials/hero'); ?>    
-    <?php get_template_part('partials/section-advantages'); ?>    
-    <?php get_template_part('partials/section-sell-easy'); ?>    
-    <?php get_template_part('partials/section-conditions'); ?>    
-    <?php get_template_part('partials/section-companies'); ?>  
-    <?php get_template_part('partials/section-testimonials'); ?>  
-    <?php get_template_part('partials/section-search_offers'); ?>
+    <?php get_template_part('template-parts/hero'); ?>    
+    <?php get_template_part('template-parts/section-advantages'); ?>    
+    <?php get_template_part('template-parts/section-sell-easy'); ?>    
+    <?php get_template_part('template-parts/section-conditions'); ?>    
+    <?php get_template_part('template-parts/section-companies'); ?>  
+    <?php get_template_part('template-parts/section-testimonials'); ?>  
+    <?php get_template_part('template-parts/section-search_offers'); ?>
 </main>
 
 <?php
